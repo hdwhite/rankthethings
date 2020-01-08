291d9ae273c6dc1ffb0e288741bd8cb63c96c20b
Harry White <hdwhite@vt.edu>
2020-01-08 01:07:03 -0500

    Cleaned up files

--- rankings.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("dbnames.inc");
-require_once($_dbconfig);
-$rawvotes = array();
-$query = $mysqli->query("SELECT winner, loser, COUNT(*) AS num FROM $lockdb GROUP BY winner, loser");
-
-?>
