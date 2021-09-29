6db16b87713e56d2fe842f387f4003f7f1176bfb
Harry White <hdwhite@vt.edu>
2021-09-29 16:45:48 -0400

    Updated vote text

--- vote.php
+++ vote.php
@@ -7,9 +7,9 @@ if(!isset($_SESSION['ranks']))
 if(!isset($_SESSION['ranks'][$week]))
 	require_once("generate_matchups.php");
 
-# The vote page for each matchup. Here we load the teams and have trhe user vote on them.
-$team1 = $_SESSION[$region][$week]->matchups[$_SESSION[$region][$week]->votes][0];
-$team2 = $_SESSION[$region][$week]->matchups[$_SESSION[$region][$week]->votes][1];
+# The vote page for each matchup. Here we load the entrants and have the user vote on them.
+$entry1 = $_SESSION['ranks'][$week]->matchups[$_SESSION['ranks'][$week]->votes][0];
+$entry2 = $_SESSION['ranks'][$week]->matchups[$_SESSION['ranks'][$week]->votes][1];
 ?>
 
 <html>
@@ -17,30 +17,32 @@ $team2 = $_SESSION[$region][$week]->matchups[$_SESSION[$region][$week]->votes][1
 		<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/analytics.php"); ?>
 		<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 		<style type="text/css">
-			@import url("lolranks.css");
+			@import url("rankthethings.css");
 		</style>
-		<title>League of Legends <?=$region ?> Power Rankings</title>
+		<title>Rank All the Things!</title>
 	</head>
 	<body>
 		<div id="wrapper">
-			<h2>Select which team you believe would win</h2>
+			<h2>Select which thing you like best</h2>
 			<form method="post" action="action.php">
-				<input type="hidden" name="from" value="<?=$region ?>">
-				<input type="hidden" name="team1" value="<?=$team1 ?>">
-				<input type="hidden" name="team2" value="<?=$team2 ?>">
+				<input type="hidden" name="week" value="<?=$week ?>">
+				<input type="hidden" name="entry1" value="<?=$team1 ?>">
+				<input type="hidden" name="entry2" value="<?=$team2 ?>">
 				<table>
 					<tr>
-						<td style="text-align:left;"><button class="team" type="submit" name="button1"><img src="images/<?=$teams[$team1]['code'] ?>.png" height="240" width="240"><br><?=$teams[$team1]['name'] ?></button></td>
+						<td style="text-align:left;"><button class="entry" type="submit" name="button1"><img src="images/<?=$entries[$entry1]['code'] ?>.png" height="240" width="240"><br><?=$entries[$entry1]['name'] ?></button></td>
 						<td width="99%"></td>
-						<td style="text-align:right;"><button class="team" type="submit" name="button2"><img src="images/<?=$teams[$team2]['code'] ?>.png" height="240" width="240"><br><?=$teams[$team2]['name'] ?></button></td>
+						<td style="text-align:right;"><button class="entry" type="submit" name="button2"><img src="images/<?=$entries[$entry2]['code'] ?>.png" height="240" width="240"><br><?=$entries[$entry2]['name'] ?></button></td>
 					</tr>
+					<?php if($haslinks == 1) { ?>
 					<tr>
-						<td style="text-align:center;"><a href="https://lol.gamepedia.com/<?=$teams[$team1]['name'] ?>" target="_blank">Gamepedia page</a></td>
+						<td style="text-align:center;"><a href="https://en.wikipedia.org/wiki/<?=$teams[$team1]['name'] ?>" target="_blank">Wikipedia page</a></td>
 						<td width="99%"></td>
-						<td style="text-align:center;"><a href="https://lol.gamepedia.com/<?=$teams[$team2]['name'] ?>" target="_blank">Gamepedia page</a></td>
+						<td style="text-align:center;"><a href="https://en.wikipedia.org/wiki/<?=$teams[$team2]['name'] ?>" target="_blank">Wikipedia page</a></td>
 					</tr>
+					<?php } ?>
 				</table>
-				<p><button class="unknown" type="submit" name="button3">I don't know enough about these teams to make a decision</button></p>
+				<p><button class="unknown" type="submit" name="button3">I don't know enough about these things to make a decision</button></p>
 			</form>
 			<p class="patreon"><a href="https://patreon.com/hdwhite" target="_blank">Support me on Patreon!</a></p>
 		</div>
