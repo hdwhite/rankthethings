07f9bbd96bd7c2b063515192eeeb4bb88cf53d0e
Harry White <hdwhite@vt.edu>
2021-10-04 01:15:51 -0400

    Updated ranking page

--- ranking.php
+++ ranking.php
@@ -1,10 +1,11 @@
 <?php
-# Here we display the rankings
-# To prevent server load, the calulations are not done every time this page is loaded
-$query = $mysqli->query("SELECT team, rating FROM $_ratingdb WHERE region='$region' AND week='$week' ORDER BY timestamp DESC, rating DESC LIMIT 10");
+// Here we display the rankings
+// To prevent server load, the calulations are not done every time this page is loaded
+require_once("data/$week/matchdata.inc");
+$query = $mysqli->query("SELECT entry, rating FROM $_ratingdb WHERE week='$week' ORDER BY timestamp DESC, rating DESC LIMIT " . count($entrants));
 $ratings = array();
 while($curteam = $query->fetch_assoc())
-	$ratings[$curteam['team']] = $curteam['rating'];
+	$ratings[$curteam['entry']] = $curteam['rating'];
 ?>
 <html>
 	<head>
@@ -19,33 +20,24 @@ while($curteam = $query->fetch_assoc())
 			.row2 { color: silver; font-weight: bold; }
 			.row3 { color: #8C7853; font-weight: bold; }
 		</style>
-		<title>Crowdsourced League of Legends Power Rankings</title>
+		<title>Rankings of the Things!</title>
 	</head>
 	<body>
 		<div id="wrapper">
-			<h2>Current Power Rankings</h2>
-			<p class="patreon">(Note: Rankings will not be accurate for the first hour or so. Please check back later for numbers that make more sense.)</p>
+			<h2><?=$topic ?></h2>
 			<table>
 				<?php
 				$i = 0;
-				foreach($ratings as $curteam => $currating)
+				foreach($ratings as $curentry => $currating)
 				{
-					foreach($teams as $t)
-						if($t['code'] ==  $curteam)
-							$fullname = $t['name'];
+					foreach($entries as $e)
+						if($e['code'] ==  $curentry)
+							$fullname = $e['name'];
 					printf("<tr class=\"row%d\"><th>%d</th><td style=\"width: 200px\">%s</td><td>%.3f</td></tr>", ++$i, $i, $fullname, $currating);
 				}
 				?>
 			</table>
-			<h2>Thank you for participating!</h2>
-			<form method="post" action="action.php">
-				<input type="hidden" name="from" value="ranking">
-				<?php if($region == "EU") { ?>
-					<button class="team" type="submit" name="button1"><img src="images/LCS.png" height="240" width="240"><br>Switch to NA</button></td>
-				<?php } elseif($region == "NA") { ?>
-					<button class="team" type="submit" name="button2"><img src="images/LEC.png" height="240" width="240"><br>Switch to EU</button></td>
-				<?php } ?>
-			</form>
+			<p class="patreon">Rankings are likely to fluctuate, especially early in the week, so be sure to check back Monday for final results and another series of votes.<1/p>
 			<p class="patreon"><a href="https://patreon.com/hdwhite" target="_blank">Support me on Patreon!</a></p>
 		</div>
 	</body>
